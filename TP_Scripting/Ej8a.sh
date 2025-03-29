1b0c8c17cbdc0602684d6a2aa920902bb31e517a
nachoperna <ignacioperna@gmail.com>
2025-03-29 14:16:14 -0300

    Ej8 resuelto con explicacion

--- TP_Scripting/Ej8a.sh
+++ TP_Scripting/Ej8a.sh
@@ -1,19 +1,19 @@
 read -p "Directorio: " directorio
-espacio=""
-n_ant=$(echo "$directorio" | grep -o '/' | wc -l)
-find "$directorio" -type d | while IFS= read -r sub; do
-	if [ $(echo "$sub" | grep -o "git" | wc -l) = 0 ]; then
-		n_act=$(echo "$sub" | grep -o '/' | wc -l)
-		if [ $n_act -gt $n_ant ]; then
+espacio="" # delimitador de directorios
+n_ant=$(echo "$directorio" | grep -o '/' | wc -l) # contabiliza profundidad
+find "$directorio" -type d | while IFS= read -r sub; do # recorre subdirectorios
+	if [ $(echo "$sub" | grep -o "git" | wc -l) = 0 ]; then # evita todas las carpetas .git
+		n_act=$(echo "$sub" | grep -o '/' | wc -l) # contabiliza profundidad actual
+		if [ $n_act -gt $n_ant ]; then # profundidad actual > profundidad anterior => tab
 			for (( i = n_ant; i < n_act; i++ )); do
 				espacio+=" "
 			done
-		elif [[ $n_act -lt $n_ant ]]; then
+		elif [[ $n_act -lt $n_ant ]]; then # profundidad actual < profundidad anterior => tab menos
 			for (( i = n_act; i < n_ant; i++ )); do
 				espacio="${espacio% }"
 			done
 		fi
-		carpeta=$(basename "$sub")
+		carpeta=$(basename "$sub") # nos quedamos solo con el nombre de la carpeta
 		echo "$espacio*$carpeta"
 		n_ant=$n_act
 	fi
