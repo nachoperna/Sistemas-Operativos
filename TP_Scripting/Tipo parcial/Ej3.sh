4c44a425dac94dccfef8cec08e0df8c01f186af3
nachoperna <ignacioperna@gmail.com>
2025-04-06 16:14:57 -0300

    arreglos menores

--- TP_Scripting/Tipo parcial/Ej3.sh
+++ TP_Scripting/Tipo parcial/Ej3.sh
@@ -1,16 +1,21 @@
-read -p "Texto: " texto
+read -p "Ruta del archivo de texto: " texto
 read -p "Numero de impresora: " nro_imp
 read -p "Tipo de pagina: " tipo_pag
 
-chmod +r $"/etc/cups/printers.conf" # permiso de lectura
 impresoras=$"/etc/cups/printers.conf" # toda la lista de impresoras del sistema
 impresora=$(cat "$impresoras" | grep "$nro_imp:$tipo_pag*")
-filas=$(echo "$impresora" | cut -d ':' -f2,3) # nos quedamos con el tercer campo correspondiente a <filas>
-caracteres=$(echo "$impresora" | cut -d ':' -f2,4) # nos quedamos con el cuatro campo correspondiente a <cols>
 
-carac_texto=$(cat "$texto" | wc -c) # caracteres totales del texto
-carac_imp=$((filas*caracteres)) # caracteres totales por pagina de la impresora
+if [[ -z "$impresora" ]]; then # pregunta si la longitud de la variable impresora es cero
+	echo "No se encontro la impresora buscada en el sistema."
+else
+	filas=$(echo "$impresora" | cut -d ':' -f3) # nos quedamos con el tercer campo correspondiente a <filas>
+	caracteres=$(echo "$impresora" | cut -d ':' -f4) # nos quedamos con el cuarto campo correspondiente a <cols>
 
-paginas_totales=$(echo "scale=2;$carac_texto/$carac_imp" | bc)
+	carac_texto=$(cat "$texto" | wc -c) # caracteres totales del texto
+	carac_imp=$((filas*caracteres)) # caracteres totales por pagina de la impresora
 
-echo "Se necesitan $paginas_totales para imprimir el contenido del archivo"
+	echo "El texto tiene $carac_texto caracteres y se pueden imprimir $carac_imp caracteres por pagina."
+	paginas_totales=$(echo "scale=2;$carac_texto/$carac_imp" | bc) # calculo con 2 decimales
+
+	echo "Se necesitan $paginas_totales paginas para imprimir el contenido del archivo"
+fi
